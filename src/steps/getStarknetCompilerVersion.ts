c72b3bda91a78ba6ee298df72dcc970ec3340149
0xmonkeyy <monkey@diamondpaws.xyz>
2022-12-17 22:04:17 +0800

    add support for 0.10.3

--- src/steps/getStarknetCompilerVersion.ts
+++ src/steps/getStarknetCompilerVersion.ts
@@ -14,7 +14,7 @@ export async function getStarknetCompilerVersion(): Promise<StarknetCompilerVers
     type: "search-list",
     name: "StarknetCompilerVersion",
     message: "Compiler version:",
-    choices: ["0.10.2", "0.10.1", "0.10.0", "0.9.1", "0.9.0", "0.8.2", "0.8.1", "0.8.0"],
+    choices: ["0.10.3", "0.10.2", "0.10.1", "0.10.0", "0.9.1", "0.9.0", "0.8.2", "0.8.1", "0.8.0"],
   });
   return userInput.StarknetCompilerVersion;
 }
--- src/types.ts
+++ src/types.ts
@@ -14,4 +14,4 @@ export type SourceCode = {
 export type networkType = "mainnet" | "testnet" | "testnet-2"
 
 export type StarknetCompilerVersion = 
-  "0.10.2" | "0.10.1" | "0.10.0" | "0.9.1" | "0.9.0" | "0.8.2" | "0.8.1" |"0.8.0"
+  "0.10.3" | "0.10.2" | "0.10.1" | "0.10.0" | "0.9.1" | "0.9.0" | "0.8.2" | "0.8.1" |"0.8.0"
